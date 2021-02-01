dd49f935a0ad06f65ed1574d303de9b696664c97
KirillYast <75496900+KirillYast@users.noreply.github.com>
2021-02-01 16:50:00 +0500

    Lesson_1
    Compleated 1-4

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var Tc = prompt('Введите Tc');
+var Tf = (9 / 5) * Tc + 32;
+alert('Tf=' + Tf);
+console.log('Tf=', Tf);
+var name = 'Василий';
+var admin = name;
+console.log('admin= "', admin, '"');
+var sum = 1000 + '108';
+console.log('Concatenation 1000 + "108"=', sum);
+var sum = '108' - (-1000);
+console.log('Sum "108" -(-1000)=', sum);
